c348ba7ebfe73a2fe24472ff97695ec5b29cd199
Freewind <nowindlee@gmail.com>
2023-02-05 19:21:31 +0800

    fix css

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ TypeScript React Space With Null Item Demo
 
 Space中的如果有null item，它们本身不会显示，但会导致出现多余的空隙
 
+官方说之所以保留是防止破坏顺序导致出错，可以通过css的方式修正
+
+
+
 ```
 npm install
 npm start
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import {Button, Space} from 'antd';
 import {Empty} from "./Empty";
+import './fix.css'
 
 export default function Hello() {
-    return <Space>
-        <Button>AAA</Button>
-        <Button>BBB</Button>
-        <Empty/>
-        <Empty/>
-        <Empty/>
-        <Empty/>
-        <Button>CCC</Button>
+    return <Space direction={"vertical"}>
+        <Space id={"row1"}>
+            <Button>AAA</Button>
+            <Button>BBB</Button>
+            <Empty/>
+            <Empty/>
+            <Empty/>
+            <Empty/>
+            <Button>CCC</Button>
+        </Space>
+        <Space id={"row2"}>
+            <Button>AAA</Button>
+            <Button>BBB</Button>
+            <Empty/>
+            <Empty/>
+            <Empty/>
+            <Empty/>
+            <Button>CCC</Button>
+        </Space>
     </Space>
 };
